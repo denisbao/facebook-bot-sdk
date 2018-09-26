bb4b933f758da19f3bbf0b2f625474a8c217703f
Denis Bao Motta <denis.tpd@gmail.com>
2018-09-26 17:15:31 -0300

    templates criados

---
+++ src/TemplatesMessage/GenericTemplate.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace CodeBot\TemplatesMessage;
+
+use CodeBot\Element\ElementInterface;
+
+
+class GenericTemplate implements TemplateInterface
+{
+
+    protected $products = [];
+    protected $recipientId;
+
+
+    public function __construct($recipientId)
+    {
+        $this->recipientId = $recipientId;
+    }
+
+    //ADICIONA BOTÕES
+    public function add(ElementInterface $element)
+    {
+        $this->products[] = $element->get();
+    }
+
+
+    //RETORNO DA MENSAGEM PARA O FACEBOOK:
+    public function message($messageText): array
+    {
+        return [
+            'recipient' => [
+                'id' => $this->recipientId
+            ],
+            'message' => [
+                'attachment' => [
+                    'type' => 'template',
+                    'payload' => [
+                        'template_type' => 'generic',
+                        'buttons' => $this->products
+                    ]
+                ]
+            ]
+        ];
+    }
+}
---
+++ src/TemplatesMessage/ListTemplate.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace CodeBot\TemplatesMessage;
+
+use CodeBot\Element\ElementInterface;
+
+
+class ListTemplate implements TemplateInterface
+{
+
+    protected $products = [];
+    protected $recipientId;
+
+
+    public function __construct($recipientId)
+    {
+        $this->recipientId = $recipientId;
+    }
+
+    //ADICIONA BOTÕES
+    public function add(ElementInterface $element)
+    {
+        $this->products[] = $element->get();
+    }
+
+
+    //RETORNO DA MENSAGEM PARA O FACEBOOK:
+    public function message($messageText): array
+    {
+        return [
+            'recipient' => [
+                'id' => $this->recipientId
+            ],
+            'message' => [
+                'attachment' => [
+                    'type' => 'template',
+                    'payload' => [
+                        'template_type' => 'list',
+                        'text' => $messageText,
+                        'buttons' => $this->products
+                    ]
+                ]
+            ]
+        ];
+    }
+}
