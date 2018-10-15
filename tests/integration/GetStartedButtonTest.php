89a552259466a3fbfcabb4f6ac87e8bce1d03f6b
Denis Bao Motta <denis.tpd@gmail.com>
2018-10-15 14:21:56 -0300

    remocao do botao iniciar

--- tests/integration/GetStartedButtonTest.php
+++ tests/integration/GetStartedButtonTest.php
@@ -9,10 +9,20 @@ class GetStartedButtonTest extends TestCase
     public function testAddGetStartedButton()
     {
         $data = (new GetStartedButton())->add('iniciar');
-        $callSendApi = new CallSendApi('EAAEditZB3cqcBAPEpqjlggSVUaDKFMELkwfYnhYSnZAyshD6WF4SulPO7sXwUZA7kiBd7jBMDHJJZBnLArOixLRuYYhrQqbaXD9kfaTgdQqZBTxHbbiUfDyLOZC38MVeJX537KYf32brqwyUdiCc306QljhVdUXbH1c2YRZCb76QQupHRDal0K7');
+        $callSendApi = new CallSendApi('COLOCAR_O_ACCESS_TOKEN');
         $result = $callSendApi->make($data, CallSendApi::URL_PROFILE);
 
         $this->assertTrue(is_string($result));
 
+    }
+
+
+    public function testRemoveGetStartedButton()
+    {
+        $data = (new GetStartedButton())->remove();
+        $callSendApi = new CallSendApi('COLOCAR_O_ACCESS_TOKEN');
+        $result = $callSendApi->make($data, CallSendApi::URL_PROFILE, 'DELETE');
+
+        $this->assertTrue(is_string($result));
     }
 }
